74183f605cb1fa9ad6aea50ea9e1c95a25e11172
wainshain <wainshain@gmail.com>
2015-04-01 21:50:23 +0800

    ddd

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -29,7 +29,7 @@ class TopicsController < ApplicationController
 
     respond_to do |format|
       if @topic.save
-        format.html { redirect_to topics_path, notice: 'Topic was successfully created.' }
+        format.html { redirect_to topics_path, notice: '' }
         format.json { render :show, status: :created, location: @topic }
       else
         format.html { render :new }
@@ -43,7 +43,7 @@ class TopicsController < ApplicationController
   def update
     respond_to do |format|
       if @topic.update(topic_params)
-        format.html { redirect_to topics_path, notice: 'Topic was successfully updated.' }
+        format.html { redirect_to topics_path, notice: '' }
         format.json { render :show, status: :ok, location: @topic }
       else
         format.html { render :edit }
@@ -57,7 +57,7 @@ class TopicsController < ApplicationController
   def destroy
     @topic.destroy
     respond_to do |format|
-      format.html { redirect_to topics_url, notice: 'Topic was successfully destroyed.' }
+      format.html { redirect_to topics_url, notice: '成功刪除留言' }
       format.json { head :no_content }
     end
   end
